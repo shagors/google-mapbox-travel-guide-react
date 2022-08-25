aaf4356faf0b2a611ce9fa99004c43998de515bc
shagors <shajjadshagor@gmail.com>
2022-08-25 20:28:13 +0600

    Header js file design and some code set and some error need make up

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Typography, InputBase, Box, Toolbar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Map Guide
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore New Places
+          </Typography>
+          <Autocomplete>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon></SearchIcon>
+              </div>
+              <InputBase
+                placeholder="Search ..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              ></InputBase>
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Header
+export default Header;
